1a3f6e851620297888edb4355557b08ae484dc2c
Juan Carlos Fernandez <Juan.Carlos.Fernandez@ricoh.es>
2025-03-10 12:23:36 +0100

    bug solved

--- src/config.py
+++ src/config.py
@@ -4,5 +4,5 @@ import os
 MODEL_NAME = os.getenv("MODEL_NAME", "llama3.2")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "nomic-embed-text")
 COLLECTION_NAME = os.getenv("COLLECTION_NAME", "simple-rag")
-PDF_PATH = os.getenv("PDF_PATH", "./data/default.pdf")
-VECTOR_DB_PATH = os.getenv("VECTOR_DB_PATH", "./data/default_vector_store")
+PDF_PATH = os.getenv("PDF_PATH", "./data/doc3.pdf")
+VECTOR_DB_PATH = os.getenv("VECTOR_DB_PATH", "./data/vector_store")
